13572f4fac6461fb424c97b5b190684b807af38e
EmmaWolfgram <ewolfgram@ucsd.edu>
2024-02-01 16:47:41 -0800

    fix arrayExamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,35 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlace1() {
+    int[] input2 = {1, 2, 3};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{3, 2, 1}, input2);
+  }
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = {1, 2, 3};
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    double[] input = {1.0, 3.0, 7.0};
+    assertEquals(5.0, ArrayExamples.averageWithoutLowest(input), 5);
+  }
+
+  @Test
+  public void testAverageWithoutLowest2() {
+    double[] input = {-5.0, -7.0, -6.0};
+    assertEquals(-5.5, ArrayExamples.averageWithoutLowest(input), 0);
   }
 }
